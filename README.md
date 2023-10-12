d9cde70cdd129209f8d613d8dabff3529d60c927
Rafael Ponieman <rafaelponieman@gmail.com>
2023-10-12 19:52:55 -0300

    Created API app skeleton with express-generator-typescript
    Added LICENSE and README files

---
+++ README.md
@@ -0,0 +1,1 @@
+# shopconnect 🛒⚡
---
+++ api/src/routes/api.ts
@@ -0,0 +1,61 @@
+import { Router } from 'express';
+import jetValidator from 'jet-validator';
+
+import Paths from '../constants/Paths';
+import User from '@src/models/User';
+import UserRoutes from './UserRoutes';
+
+
+// **** Variables **** //
+
+const apiRouter = Router(),
+  validate = jetValidator();
+
+
+// ** Add UserRouter ** //
+
+const userRouter = Router();
+
+// Get all users
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+userRouter.get(
+  Paths.Users.Get,
+  UserRoutes.getAll,
+);
+
+// Add one user
+userRouter.post(
+  Paths.Users.Add,
+  validate(['user', User.isUser]),
+  UserRoutes.add,
+);
+
+// Update one user
+userRouter.put(
+  Paths.Users.Update,
+  validate(['user', User.isUser]),
+  UserRoutes.update,
+);
+
+// Delete one user
+userRouter.delete(
+  Paths.Users.Delete,
+  validate(['id', 'number', 'params']),
+  UserRoutes.delete,
+);
+
+// Add UserRouter
+apiRouter.use(Paths.Users.Base, userRouter);
+
+
+// **** Export default **** //
+
+export default apiRouter;
