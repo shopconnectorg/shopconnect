3bf51d370d89967a58f285b07ee1ecf65232ed60
LeonardoRyuta <leonardo.ryuta05@gmail.com>
2023-10-15 22:45:08 +0200

    web shop 1

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,7 +4,7 @@ import listing from "../data/mockListings.json"
 
 export default function Home() {
   return (
-    <div className="flex justify-evenly bg-base-100 flex-wrap gap-4">
+    <div className="flex justify-evenly bg-base-200 flex-wrap gap-4 p-6">
       {
         listing.map((listing, index) => (
           <Card id={listing.id} name={listing.name} description={listing.description} price={listing.price} tags={listing.tags} image={listing.image} key={index}/>
--- web/components/card.tsx
+++ web/components/card.tsx
@@ -6,17 +6,24 @@ import { useRouter } from "next/navigation"
 export default function Card({id, name, description, price, tags, image}: Listing) {
   const router = useRouter();
 
+  const addToCart = (e:any) => {
+    e.stopPropagation();
+    localStorage.setItem("cart", JSON.stringify([...JSON.parse(localStorage.getItem("cart") || "[]"), {id, name, description, price, tags, image}]))
+    router.refresh();
+  }
+    
+
   return (
-    <div className="card w-1/4 bg-base-200 shadow-xl p-4">
+    <div className="card w-1/4 bg-base-300 shadow-xl p-4 cursor-pointer hover:scale-105 transition-all" onClick={()=>{router.push(`/item/${id}`)}}>
       <figure className="h-2/3">
-        <img src={image} alt="Shoes" />
+        <img className="rounded-xl" src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
+        <h2 className="card-title hover:underline">{name}</h2>
         <p>{description}</p>
         <div className="card-actions justify-end items-center">
-          <p>{price}</p>
-          <button className="btn btn-primary" onClick={()=>{router.push(`/item/${id}`)}}>Buy Now</button>
+          <p>${price}</p>
+          <button className="btn btn-primary" onClick={(e)=>{addToCart(e)}}>Add to Cart</button>
         </div>
       </div>
     </div>
