cda88c1475881c0c67361c39ef6b186a668b7ff9
Rafael Ponieman <rafaelponieman@gmail.com>
2023-10-20 13:32:32 -0300

    Added ShopConnect store frontend plugin

--- store1/app/layout.tsx
+++ store1/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import ShopConnectPlugin from '@/shopconnect-plugin/components/ShopConnectPlugin';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" data-theme="dracula">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <ShopConnectPlugin />
+      </body>
     </html>
   )
 }
---
+++ store1/shopconnect-plugin/components/ShopConnectPlugin.tsx
@@ -0,0 +1,3 @@
+export default () => (
+  <div id="shopconnect-plugin" />
+);
