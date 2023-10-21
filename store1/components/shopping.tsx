959bd6615c3891ad8e96a2b35052d37bfa3d6cb3
LeonardoRyuta <leonardo.ryuta05@gmail.com>
2023-10-21 21:23:14 +0200

    checkout navigation fix

--- store1/components/shopping.tsx
+++ store1/components/shopping.tsx
@@ -143,7 +143,7 @@ export default function ShoppingCart({open, setOpen}: {open: boolean, setOpen: (
                       <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                       <div className="mt-6">
                         <Link
-                          href="checkout"
+                          href="/checkout"
                           className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                         >
                           Checkout
